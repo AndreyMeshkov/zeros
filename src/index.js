d588aa74f8394d632271a161a74b20c1989adc10
AndreyMeshkow <meshkow.andrei@gmail.com>
2019-10-19 17:17:11 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function zeros(expression) {
   // your solution
+  let array = expression.split("*");
+  let result = 0;
+  let numberTwo = 0;
+  let numberFive = 0;
+  
+    for (var i = 0; i < array.length; i++);{
+      var partArray = array[i];
+  
+      if (partArray.indexOf("!!") > -1) {
+      stepFactorial = 2;
+      valueFactorial = + partArray.slice(0,-2);
+    } else {
+      stepFactorial = 1;
+      valueFactorial = + partArray.slice(0,-1);
+    }
+    if (stepFactorial === 1) {
+      for (let j = 1 ; j <= valueFactorial ; j++) {
+      if (j % 2 === 0) { 
+        numberTwo++
+      }
+      if (j % 5 === 0) {
+        numberFive++
+      }
+      
+      }
+    }
+    if (stepFactorial === 2) {
+      if (valueFactorial % 2 === 0) {
+        for(let k = 2; k <= valueFactorial; k += 2) {
+          if (k % 2 === 0) { 
+            numberTwo++
+          }
+        }
+      }
+      else {
+        for(let l = 1; l <= valueFactorial; l += 2) {
+          if (l % 5 === 0) {
+            numberFive++
+        }
+      }
+    }
+  }
+}
+result += Math.min(numberTwo, numberFive);
+return result;
+}
+
+function newFunction(partArray) {
+  return partArray.indexOf("!!") > -1;
 }
